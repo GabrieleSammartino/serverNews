3b13bfa2298f3763b69d8fd00151735ed60588a0
Gabriele Sammartino <Gabriele.sammartino@gmail.com>
2017-06-16 09:56:04 +0200

    0.2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 var index = require('./routes/index');
 var news = require('./routes/news');
